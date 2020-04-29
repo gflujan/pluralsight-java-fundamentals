997c2ddec689e559c19dd704da55922309eef4e4
Neo Andres <gfl.devo@gmail.com>
2020-04-28 20:58:22 -0700

    update HelloWorld to use comments

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello World One"); // this is a comment
+     // System.out.println("Hello World Two");
+    System.out.println("Hello World Three");
+  }
 }

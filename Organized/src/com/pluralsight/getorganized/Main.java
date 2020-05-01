35a0af014dccff5366ddd403741f9941822ab3c4
Neo Andres <gfl.devo@gmail.com>
2020-04-30 19:29:54 -0700

    demoing operator precedence

--- Organized/src/com/pluralsight/getorganized/Main.java
+++ Organized/src/com/pluralsight/getorganized/Main.java
@@ -3,6 +3,21 @@ package com.pluralsight.getorganized;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello Get Organized");
+    // System.out.println("Hello Get Organized");
+
+    int valA = 21;
+    int valB = 6;
+    int valC = 3;
+    int valD = 1;
+
+    int result1 = valA - valB / valC;
+    int result2 = (valA - valB) / valC;
+    int result3 = valA / valC * valD + valB;
+    int result4 = valA / (valC * (valD + valB));
+
+    System.out.println(result1);
+    System.out.println(result2);
+    System.out.println(result3);
+    System.out.println(result4);
   }
 }

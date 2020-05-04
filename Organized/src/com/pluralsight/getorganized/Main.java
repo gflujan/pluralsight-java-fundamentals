2a27cb57fd64858048e876bd768152b0d0350c8a
Neo Andres <gfl.devo@gmail.com>
2020-05-03 19:52:18 -0700

    add demo for L29: Nested `if` statements

--- Organized/src/com/pluralsight/getorganized/Main.java
+++ Organized/src/com/pluralsight/getorganized/Main.java
@@ -26,30 +26,47 @@ public class Main {
     /* -------------------------------------------------------------------------- */
     /* L25: DEMO: Type Conversion */
     /* -------------------------------------------------------------------------- */
-    float floatVal = 1.0f;
-    double doubleVal = 4.0d;
-    byte byteVal = 7;
-    short shortVal = 7;
-    long longVal = 5;
+    // float floatVal = 1.0f;
+    // double doubleVal = 4.0d;
+    // byte byteVal = 7;
+    // short shortVal = 7;
+    // long longVal = 5;
+    //
+    // short result1a = byteVal; // you can also explicitly cast the `byteVal` to be `(short)`
+    //
+    // short result2a = longVal; // wrong way of doing it
+    // short result2b = (short) longVal;
+    //
+    // short result3a = byteVal - longVal; // wrong way of doing it
+    // short result3b = (short) (byteVal - longVal);
+    //
+    // long result4a = longVal - floatVal; // wrong way of doing it
+    // float result4b = longVal - floatVal;
+    //
+    // float result5a = longVal - doubleVal; // wrong way of doing it
+    // double result5b = longVal - doubleVal;
+    //
+    // long result6a = shortVal - longVal + floatVal; // wrong way of doing it
+    // long result6b = shortVal - longVal + floatVal + doubleVal; // wrong way of doing it
+    // long result6c = (long) (shortVal - longVal + floatVal + doubleVal);
 
-    short result1a = byteVal; // you can also explicitly cast the `byteVal` to be `(short)`
+    // System.out.println("Success");
 
-    short result2a = longVal; // wrong way of doing it
-    short result2b = (short) longVal;
+    /* -------------------------------------------------------------------------- */
+    /* L29: DEMO: Nested `if` Statements */
+    /* -------------------------------------------------------------------------- */
+    float students = 0.0f;
+    float rooms = 4.0f;
 
-    short result3a = byteVal - longVal; // wrong way of doing it
-    short result3b = (short) (byteVal - longVal);
+    if (students > 0.0f) {
+      if (rooms > 0.0f) {
+        System.out.println(students / rooms);
+      }
+    } else {
+      System.out.println("No students");
+    }
 
-    long result4a = longVal - floatVal; // wrong way of doing it
-    float result4b = longVal - floatVal;
-
-    float result5a = longVal - doubleVal; // wrong way of doing it
-    double result5b = longVal - doubleVal;
-
-    long result6a = shortVal - longVal + floatVal; // wrong way of doing it
-    long result6b = shortVal - longVal + floatVal + doubleVal; // wrong way of doing it
-    long result6c = (long) (shortVal - longVal + floatVal + doubleVal);
-
-    System.out.println("Success");
+    System.out.println();
+    System.out.println("** end program **");
   }
 }

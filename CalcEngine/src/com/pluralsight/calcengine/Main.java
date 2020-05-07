76ba76c3064fbed6337c722ecc2da23ae6d2c5b0
Neo Andres <gfl.devo@gmail.com>
2020-05-06 21:28:11 -0700

    add initial logic for CalcEngine

--- CalcEngine/src/com/pluralsight/calcengine/Main.java
+++ CalcEngine/src/com/pluralsight/calcengine/Main.java
@@ -6,5 +6,24 @@ public class Main {
         /* -------------------------------------------------------------------------- */
         /* L32: DEMO: CalcEngine */
         /* -------------------------------------------------------------------------- */
+        double val1 = 100.0d;
+        double val2 = 50.0d;
+        double result;
+        char opCode = 'd';
+
+        if (opCode == 'a') {
+            result = val1 + val2;
+        } else if (opCode == 's') {
+            result = val1 - val2;
+        } else if (opCode == 'd') {
+            result = val1 / val2;
+        } else if (opCode == 'm') {
+            result = val1 * val2;
+        } else {
+            System.out.println("ERROR - Invalid 'opCode'");
+            result = 0.0d;
+        }
+
+        System.out.println(result);
     }
 }

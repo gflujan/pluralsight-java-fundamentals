3b807b7d83e7a9307f5e3b446ccae77b79356b60
Neo Andres <gfl.devo@gmail.com>
2020-05-15 23:17:22 -0700

    update calc to use arrays, loops and switch statements

--- CalcEngine/src/com/pluralsight/calcengine/Main.java
+++ CalcEngine/src/com/pluralsight/calcengine/Main.java
@@ -6,30 +6,65 @@ public class Main {
         /* -------------------------------------------------------------------------- */
         /* L32: DEMO: CalcEngine */
         /* -------------------------------------------------------------------------- */
-        double val1 = 100.0d;
-        double val2 = 50.0d;
-        double result;
-        char opCode = 'd';
+        // double val1 = 100.0d;
+        // double val2 = 50.0d;
+        // double result;
+        // char opCode = 'd';
+        //
+        // if (opCode == 'a') {
+        //     result = val1 + val2;
+        // } else if (opCode == 's') {
+        //     result = val1 - val2;
+        // } else if (opCode == 'd') {
+        //     result = val2 != 0.0d ? val1 / val2 : 0.0d;
+        //
+        //     // if (val2 != 0.0d) {
+        //     //     result = val1 / val2;
+        //     // } else {
+        //     //     result = 0.0d;
+        //     // }
+        // } else if (opCode == 'm') {
+        //     result = val1 * val2;
+        // } else {
+        //     System.out.println("ERROR - Invalid 'opCode'");
+        //     result = 0.0d;
+        // }
 
-        if (opCode == 'a') {
-            result = val1 + val2;
-        } else if (opCode == 's') {
-            result = val1 - val2;
-        } else if (opCode == 'd') {
-            result = val2 != 0.0d ? val1 / val2 : 0.0d;
+        // System.out.println(result);
 
-            // if (val2 != 0.0d) {
-            //     result = val1 / val2;
-            // } else {
-            //     result = 0.0d;
-            // }
-        } else if (opCode == 'm') {
-            result = val1 * val2;
-        } else {
-            System.out.println("ERROR - Invalid 'opCode'");
-            result = 0.0d;
+        /* -------------------------------------------------------------------------- */
+        /* L37: DEMO: w/ Arrays, Loops & Switch */
+        /* -------------------------------------------------------------------------- */
+        double[] leftVals = { 100.0d, 25.0d, 225.0d, 11.0d };
+        double[] rightVals = { 50.0d, 92.0d, 17.0d, 3.0d };
+        char[] opCodes = { 'd', 'a', 's', 'm' };
+        double[] results = new double[opCodes.length];
+
+        // Using a standard "for" loop
+        for (int i = 0; i < opCodes.length; i += 1) {
+            switch (opCodes[i]) {
+                case 'a':
+                    results[i] = leftVals[i] + rightVals[i];
+                    break;
+                case 'd':
+                    results[i] = rightVals[i] == 0.0d ? 0.0d : leftVals[i] / rightVals[i];
+                    break;
+                case 'm':
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                case 's':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+                default:
+                    System.out.println("Error - Invalid 'opCode'");
+                    results[i] = 0.0d;
+                    break;
+            }
         }
 
-        System.out.println(result);
+        for (double currResult : results) {
+            System.out.print("result = ");
+            System.out.println(currResult);
+        }
     }
 }

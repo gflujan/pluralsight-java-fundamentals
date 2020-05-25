9197ef932e058f9cd989e07228fc99e1d4a90313
Neo Andres <gfl.devo@gmail.com>
2020-05-24 19:04:52 -0700

    adding SomeClass to test connecting classes

---
+++ CalcEngine/src/com/pluralsight/calcengine/SomeClass.java
@@ -0,0 +1,14 @@
+package com.pluralsight.calcengine;
+
+/**
+ * @author Metal Gabe
+ * @created 24-May-2020 @ 18:59
+ */
+
+public class SomeClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
